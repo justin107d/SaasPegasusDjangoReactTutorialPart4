6ec3877ee0bf6cc8f86f2b519bf11a0db418d9fc
Justin <justin107d@gmail.com>
2022-04-08 22:45:22 -0400

    Update for use in React 18
    ReactDOM is being depreciated in React 18.  A comment was entered so the user can quickly adjust if they so choose.

--- assets/index.js
+++ assets/index.js
@@ -8,8 +8,17 @@ import App from "./App";
 const DEPARTMENT_CHOICES = JSON.parse(document.getElementById('department-choices').textContent);
 console.log("Employment: ", DEPARTMENT_CHOICES);
 
+// This will soon be depreciated in React 18
 ReactDOM.render(
   <App />,
   document.getElementById('js-framework-home')
 );
 
+// Use this instead.
+//import { createRoot } from 'react-dom/client';
+//import React from 'react';
+//import App from './App';
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<App/>);
